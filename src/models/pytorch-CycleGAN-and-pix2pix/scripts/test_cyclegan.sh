5b3b3cc2abbddd61417e95d4c5f7f81bd1dfb4b2
Sofiia Petryshyn <sofiia.petryshyn@avenga.com>
2022-08-14 17:17:17 +0200

    scripts changed.

--- src/models/pytorch-CycleGAN-and-pix2pix/scripts/test_cyclegan.sh
+++ src/models/pytorch-CycleGAN-and-pix2pix/scripts/test_cyclegan.sh
@@ -1,2 +1,3 @@
 set -ex
-python test.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan --phase test --no_dropout
+python test.py --dataroot ./datasets/dance2energy128 --name dance2energy_cyclegan128 --model cycle_gan --phase test --no_dropout
+
--- src/models/pytorch-CycleGAN-and-pix2pix/scripts/train_cyclegan.sh
+++ src/models/pytorch-CycleGAN-and-pix2pix/scripts/train_cyclegan.sh
@@ -1,2 +1,2 @@
 set -ex
-python train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan --pool_size 50 --no_dropout
+python train.py --dataroot ./datasets/dance2energy128 --name dance2energy_cyclegan128 --model cycle_gan --pool_size 50 --no_dropout --load_size 128 --crop_size 128
